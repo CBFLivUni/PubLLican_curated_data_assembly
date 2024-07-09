12f761800146fea91486c1edeee814b49047bdd5
JordanTzvetkov <149078174+JordanTzvetkov@users.noreply.github.com>
2024-07-09 14:53:27 +0100

    added a list of PMIDs used in the curated data generation

--- scripts/get_PMIDs_list.py
+++ scripts/get_PMIDs_list.py
@@ -8,14 +8,15 @@ import pandas as pd
 
 PMIDs_data_dir = os.path.join("..", "output")
 merged_gaf_file = os.path.join(PMIDs_data_dir, "merged_gaf.tsv")
+output_txt_file = os.path.join(PMIDs_data_dir, "unique_pmids_from_curated_dataset.txt")
 
 # read in merged_gaf.tsv column with pubmed IDs as a list
 df = pd.read_csv(merged_gaf_file, sep='\t', comment='!', header=None)
 
-# Assuming the PubMed IDs are in the 6th column (index 5)
+# PMIDs are in the 6th column (index 5)
 pubmed_ids = df[5].astype(str)  # Convert to string to handle potential float values
 
-# Remove 'PMID:' prefix and strip whitespace
+# Remove 'PMID:' prefix and strip whitespace - this was done in prev script but oh well
 pubmed_ids = pubmed_ids.str.replace("PMID:", "").str.strip()
 
 # Get unique PMIDs
@@ -25,4 +26,12 @@ unique_pmids = pubmed_ids.unique()
 unique_pmids_list = unique_pmids.tolist()
 
 # Print the list of unique PMIDs
-print(unique_pmids_list)
+print(len(unique_pmids_list))
+print(len(pubmed_ids))
+
+# Write the list of unique PMIDs to a .txt file
+with open(output_txt_file, 'w', encoding="UTF-8") as file:
+    for pmid in unique_pmids_list:
+        file.write(f"{pmid}\n")
+
+print(f"Unique PMIDs written to {output_txt_file}")
